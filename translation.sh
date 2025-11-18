3bcdab4da2572bb1a933560e8f9ba28f3f00d7ce
mayumi00 <42399141+mayumi00@users.noreply.github.com>
2025-11-18 17:54:10 +0900

    Update translation.sh

--- translation.sh
+++ translation.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-line_count=$(oc get namespace -o custom-columns=":metadata.name" | egrep -c "showroom-user")
-
-for ((i=1; i<=line_count; i++)); do
-    NS="showroom-user"$i
-    oc scale -n $NS deploy/showroom --replicas=0
-    sleep 1
-    oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/mayumi00/ocp4-getting-started-showroom"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
-    oc scale -n $NS deploy/showroom --replicas=1
-done
+NS=$(oc get namespace -o custom-columns=":metadata.name" | grep "showroom")
+echo "show room roject is " $NS
+oc scale -n $NS deploy/showroom --replicas=0
+sleep 1
+oc patch -n $NS deploy/showroom --patch='{"spec":{"template":{"spec":{"containers": [{"name": "content","env": [{"name": "GIT_REPO_URL", "value": "https://github.com/mayumi00/ocp4-getting-started-showroom"}, {"name": "GIT_REPO_REF", "value": "main"}]}]}}}}'
+oc scale -n $NS deploy/showroom --replicas=1
